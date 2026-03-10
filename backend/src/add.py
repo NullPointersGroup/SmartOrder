a0f310d13016f9b01816abb3783ae322d804d5ac
Marco Brunello <markobrunello2010@gmail.com>
2026-03-10 14:00:19 +0100

    aggiunto dockerfile con principio di fastapi

--- backend/src/add.py
+++ backend/src/add.py
@@ -1,26 +1,28 @@
 from beartype import beartype
 
+
 @beartype
 def add(x: int, y: int) -> int:
-   """
-   @brief Brief description
-   @param Type Description
-   @raise ExceptionType Condition or description
-   @bug  actual problems
-   @return Type Description
-   @req RF-OB_02
-   """
-   print("Ciao")
-   return x + y
+    """
+    @brief Brief description
+    @param Type Description
+    @raise ExceptionType Condition or description
+    @bug  actual problems
+    @return Type Description
+    @req RF-OB_02
+    """
+    print("Ciao")
+    return x + y
+
 
 def subtract(x: int, y: int) -> int:
-   """
-   @brief Brief description
-   @param Type Description
-   @raise ExceptionType Condition or description
-   @bug  actual problems
-   @return Type Description
-   @req RF-OB_03
-   """
-   print("Ciao")
-   return x - y
+    """
+    @brief Brief description
+    @param Type Description
+    @raise ExceptionType Condition or description
+    @bug  actual problems
+    @return Type Description
+    @req RF-OB_03
+    """
+    print("Ciao")
+    return x - y
