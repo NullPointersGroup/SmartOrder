f04faecf8efb6f012da17ee346c829d9e26bc92e
every-go <matteoevery498@gmail.com>
2026-03-25 10:47:10 +0100

    Tieni Lisa

--- backend/src/db/models.py
+++ backend/src/db/models.py
@@ -7,8 +7,7 @@ from sqlmodel import Field, SQLModel
 class Utente(SQLModel, table=True):
     __tablename__: ClassVar[str] = "utentiweb"
 
-    # TODO nel database username ha lunghezza massima 24, è da cambiare qui?
-    username: Optional[str] = Field(default=None, max_length=255, primary_key=True)
+    username: Optional[str] = Field(default=None, max_length=24, primary_key=True)
     descrizione: Optional[str] = Field(default=None, max_length=80)
     email: Optional[str] = Field(default=None, max_length=255)
     password: Optional[str] = Field(default=None, max_length=60)
