2eee9d12df3f5925f34423fc0f9674e747c8668a
every-go <matteoevery498@gmail.com>
2026-04-12 05:18:12 +0200

    Fix Sonar

--- backend/src/dependencies.py
+++ backend/src/dependencies.py
@@ -3,15 +3,3 @@ from src.vec.EmbeddedCatalogService import EmbeddedCatalogService
 
 catalog_repo: CatalogRepoAdapter | None = None
 embedded_catalog_service: EmbeddedCatalogService | None = None
-
-
-# def get_catalog_repo() -> CatalogRepoAdapter:
-#     if catalog_repo is None:
-#         raise RuntimeError("catalog_repo non inizializzato")
-#     return catalog_repo
-
-
-# def get_embedded_catalog_service() -> EmbeddedCatalogService:
-#     if embedded_catalog_service is None:
-#         raise RuntimeError("embedded_catalog_service non inizializzato")
-#     return embedded_catalog_service
